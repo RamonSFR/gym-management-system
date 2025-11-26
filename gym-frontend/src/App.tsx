663ec82443cb8455a7ef087e1d7997d199cbe121
RamonSFR <ramonsaviofontesrocha@gmail.com>
2025-11-26 08:59:51 -0300

    router-dom setup

--- gym-frontend/src/App.tsx
+++ gym-frontend/src/App.tsx
@@ -1,13 +1,14 @@
-import Login from "./pages/Login"
-import GlobalStyles from "./pages/Styles/GlobalStyles"
+import { BrowserRouter } from 'react-router-dom'
+
+import GlobalStyles from './pages/Styles/GlobalStyles'
+import AppRoutes from './routes'
 
 function App() {
-
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyles />
-      <Login />
-    </>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
---
+++ gym-frontend/src/routes.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from 'react-router-dom'
+
+import Login from './pages/Login'
+
+const AppRoutes = () => (
+    <Routes>
+        <Route path='/' element={<Login />} />
+    </Routes>
+)
+
+export default AppRoutes
