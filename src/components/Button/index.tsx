c521ac502324aa6efd0ae1b99977f333197e48c7
RamonSFR <ramonsaviofontesrocha@gmail.com>
2025-12-03 22:00:10 -0300

    a

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,25 @@
+import type { JSX } from 'react'
+
+import * as S from './styles'
+
+type Props = {
+  children: string | JSX.Element
+  type?: 'button' | 'submit' | 'reset'
+  onClick?: () => void
+  disabled?: boolean
+}
+
+const Button = ({ children, type = 'button', onClick, disabled }: Props) => {
+  return (
+    <S.Btn
+      className={disabled ? 'disabled' : ''}
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </S.Btn>
+  )
+}
+
+export default Button
